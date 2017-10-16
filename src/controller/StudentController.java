37c6cc3c16f7f74ac4d2627d2de470f267067b17
liujiuming <793923698@qq.com>
2017-10-16 13:37:10 +0800

    Revert "Revert "Initial commit""
    This reverts commit 5c15834bbb6229fd52f4c72cd9f3ea50f96f199c.

---
+++ src/controller/StudentController.java
@@ -0,0 +1,91 @@
+package controller;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import entity.Squad;
+import entity.Student;
+import service.StudentService;
+
+@Controller
+public class StudentController {
+	
+	@Autowired
+	StudentService stu;
+	
+	@RequestMapping(value = "student")
+	public ModelAndView backgroundshow(HttpServletRequest request) {
+		
+		ModelAndView mv = new ModelAndView("student/student");
+		
+	    List<Student> student =	stu.selectStudent();
+	    List<Student> stud = stu.joinStudent();
+	    for (int i = 0; i < stud.size(); i++) {
+	    	List<Squad> squ = stud.get(i).getSquid();
+	    	for (int j = 0; j < squ.size(); j++) {
+	    		System.out.println(squ.get(j).getName());
+			}
+	    	System.out.println(stud.get(i).getName());
+	    	System.out.println(stud.get(i).getAge());
+	    	System.out.println(stud.get(i).getNumber());
+	    	System.out.println(stud.get(i).getPatriarch());
+		}
+		mv.addObject("student",student);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value = "modStudent")
+	public ModelAndView modStudent(HttpServletRequest request) {
+		
+		ModelAndView mv = new ModelAndView("student/ModStudent");
+		
+		return mv;
+	}
+	
+	@RequestMapping(value = "addStudent")
+	public String addStudent(HttpServletRequest request) {
+		String name = null;
+		String sex = null;
+		String patriarch = null;
+		try {
+			name = new String( request.getParameter("name").getBytes("ISO-8859-1"),"utf-8");
+			sex = new String( request.getParameter("sex").getBytes("ISO-8859-1"),"utf-8");
+			patriarch = new String( request.getParameter("patriarch").getBytes("ISO-8859-1"),"utf-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int age  = Integer.parseInt(request.getParameter("age"));
+		int number = Integer.parseInt(request.getParameter("number"));
+		int a = stu.addStudent(name, age, sex, number, patriarch);
+		System.out.println(a);
+		return "redirect:student";
+	}
+	
+	@RequestMapping(value = "delcetStudent")
+	public String delectStudent(HttpServletRequest request,int id) {
+		
+		int a = stu.delcetStudent(id);
+		System.out.println(a);
+		return "redirect:student";
+	}
+	
+	@RequestMapping(value = "updeatStudent")
+	public ModelAndView updeatStudent(HttpServletRequest request,int id) {
+		
+		ModelAndView mv = new ModelAndView("student/ModStudent");
+		List<Student> student =  stu.selectWhereId(id);
+		mv.addObject("student",student);
+		mv.addObject("id",id);
+		
+		return mv;
+	}
+}
